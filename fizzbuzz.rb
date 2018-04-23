31e5770361f53c295752cd8bee9f0351be627483
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-04-23 18:24:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,14 @@
+def fizzbuzz(n)
+  if n / 3 == TRUE
+    puts "Fizz"
+    elsif n / 5 == TRUE
+      puts "Buzz"
+      elsif n / 3 && n / 5 == TRUE
+      puts "FizzBuzz"
+    else
+      puts "nil"
+    end
+
 var fizz_3 = fizzbuzz(3)
 var fizz_5 = fizzbuzz(5)
 var fizz_15 = fizzbuzz(15)
