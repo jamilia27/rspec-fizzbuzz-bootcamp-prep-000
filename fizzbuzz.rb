070eadce762f2fff17124c3a5e21d07d288e7209
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-04-23 18:48:53 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(int)
+  if int % 3 == 0 
+    puts "Fizz"
+    elsif int % 5 == 0 
+      puts "Buzz"
+      elsif int % 3 && int % 5 == 0 
+        puts "FizzBuzz"
+      else
+        puts "nil"
+      end
+    end
+  end
+  
