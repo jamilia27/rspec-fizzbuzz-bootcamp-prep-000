711ac9afb4efe153e839b38fb67c12139962a7d1
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-04-23 19:08:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if (n % 3 && n % 5)
+  if (int % 3 && int % 5 == 0)
     puts "FizzBuzz"
-    elsif  (n % 3)
+    elsif  (int % 3 == 0)
     puts "Fizz"
-    elsif (n % 5)
+    elsif (int % 5 == 0)
     puts "Buzz"
   else
 end
